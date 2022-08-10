e962861cd59e2d21b5522d9db3be75fe85df7457
kimnewbie <gracecancho@gmail.com>
2022-08-10 11:27:04 +0900

    Commit before eject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import { Routes } from '../node_modules/react-router-dom/index';
+import Menu from './components/Menu';
+import BluePage from './pages/BluePage';
+import RedPage from './pages/RedPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <hr />
+      <Routes>
+        <Route exact path="/red" element={<RedPage />} />
+        <Route exact path="/blue" element={<BluePage />} />
+      </Routes>
     </div>
   );
 }
